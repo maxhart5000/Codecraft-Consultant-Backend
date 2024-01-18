8121d26de18aadf15d057baf6956ee1486d6d890
Max Hart <maxhart5000@icloud.com>
2024-01-18 18:46:34 +0800

    Initial commit. Creating a backend with java and spring-boot to connect to my front end angular application. Creating two entities, product and product category. SQL script creating 100 template products to store on the db along with 4 different product categories. REST endpoints used to access the products stored on the db. The backend has a RestConfig class that restricts access to the db through read only.

---
+++ src/main/java/com/hartcode/ecommerce/SpringBootECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.hartcode.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hartcode/ecommerce/SpringBootECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hartcode.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
