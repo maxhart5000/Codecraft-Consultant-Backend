5406fc7094fc8bdba18acdd00eb0c29e807029cf
Max Hart <maxhart5000@icloud.com>
2024-02-11 23:14:12 +0900

    Integrating stripe payment functionality into the backend application. Configuring stripe payment to use the test environment.

--- src/main/java/com/hartcode/ecommerce/controller/CheckoutController.java
+++ src/main/java/com/hartcode/ecommerce/controller/CheckoutController.java
@@ -1,15 +1,26 @@
 package com.hartcode.ecommerce.controller;
 
+import com.hartcode.ecommerce.dto.PaymentInfo;
 import com.hartcode.ecommerce.dto.Purchase;
 import com.hartcode.ecommerce.dto.PurchaseResponse;
 import com.hartcode.ecommerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("api/checkout")
 public class CheckoutController {
 
+    private final Logger logger = Logger.getLogger(getClass().getName());
     private final CheckoutService checkoutService;
 
     @Autowired
@@ -23,5 +34,15 @@ public class CheckoutController {
         PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
 
         return purchaseResponse;
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+
+        logger.info("paymentInfo.amount: " + paymentInfo.getAmount() + ", paymentInfo.currency: " + paymentInfo.getCurrency());
+
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+        String paymentString = paymentIntent.toJson();
+        return new ResponseEntity<>(paymentString, HttpStatus.OK);
     }
 }
--- src/main/java/com/hartcode/ecommerce/service/CheckoutService.java
+++ src/main/java/com/hartcode/ecommerce/service/CheckoutService.java
@@ -1,8 +1,13 @@
 package com.hartcode.ecommerce.service;
 
+import com.hartcode.ecommerce.dto.PaymentInfo;
 import com.hartcode.ecommerce.dto.Purchase;
 import com.hartcode.ecommerce.dto.PurchaseResponse;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 public interface CheckoutService {
     PurchaseResponse placeOrder(Purchase purchase);
+
+    PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException;
 }
